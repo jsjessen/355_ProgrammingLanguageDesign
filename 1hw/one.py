6e61e425f563d40aeefbef56794a8885e356d8e5
James Jessen <jsjessen@gmail.com>
2015-01-25 19:07:10 -0800

    refined testing and corrected crypto letter mapping

--- 1hw/one.py
+++ 1hw/one.py
@@ -1,103 +1,130 @@
+#!/usr/bin/env python3
+
 # James Jessen
 # 10918967
 # CptS 355
-# Intended for Linux
+
+# Developed for/on Linux, specifically Ubuntu 14.10 32-bit
+
+#-------------------------------------------------------------------------------
 
 import operator
 
-debugging = True
+#debugging = True
+debugging = False
 
-def debug(*s): if debugging: print(*s)
+def debug(*s):
+    """A clone of print(), toggleable with the boolean variable debugging"""
+    if debugging: print(*s)
 
-#debug("This is debug output")
+
+def test(i, o, expected):
+    """Runs tests on a function given a list of inputs and expected outputs"""
+    if (o != expected):
+        print('=' * 78)
+        print(i)
+        print('-' * 78)
+        print("Output:")
+        print(o)
+        print('.' * 78)
+        print("Expected:")
+        print(expected)
 
 
-# The text of a message can be hidden by applying a translation function to each character.
-# Define a function makettable(s1,s2) that returns a dictionary such that each character in s1 is
-# mapped to the character at the corresponding position in s2. You may assume that the characters in
-# s1 are unique and that the two strings are the same length. (When I say "you may assume X" it means
-# that your code does not have to check whether X holds or not). So for example,
-# makettable('abcdefg', 'gfedcba') returns {'a': 'g', 'c': 'e', 'b': 'f', 'e':
-# 'c', 'd': 'd', 'g': 'a', 'f': 'b'}
+# The text of a message can be hidden by applying a translation function to each character. Define a function makettable(s1,s2) that returns a dictionary such that each character in s1 is mapped to the character at the corresponding position in s2. You may assume that the characters in s1 are unique and that the two strings are the same length. (When I say "you may assume X" it means that your code does not have to check whether X holds or not).
+# So for example, makettable('abcdefg', 'gfedcba')
+# returns {'a': 'g', 'c': 'e', 'b': 'f', 'e': 'c', 'd': 'd', 'g': 'a', 'f': 'b'}
 
 def makettable(s1,s2):
-    """Makes a table mapping string s1 to string s2"""
-    table = {}
-    for c1,c2 in s1,s2
-       table[c1] = c2
-    return table
+    """Makes a translation table mapping string s1 to string s2"""
+    debug('makettable: ' + '"'+s1+'"' + ' -> ' + '"'+s2+'"')
+    ttable = {}
+    for c1,c2 in zip(s1,s2):
+        debug(c1,c2)
+        ttable[c1] = c2
+    return ttable
 
 
-# Now, define a function trans(ttable, s). trans translates its string argument, s, according to its
-# translation table argument, ttable. Argument ttable is, of course, a dictionary returned by the
-# makettable function, and the result of trans is obtained by replacing each character, c, of s by
-# ttable[c], if c is amongst the keys of the ttable. If however c is not in ttable.keys() then it is
-# unchanged in the output. This is all harder to describe than to do! To easily look up k in dictionary, D,
-# specifying that the same character is to be used if it is not present as a key in the dictionary you can use
-# translation = D.get(k,k) # use help({}.get) to see more about get
+# Now, define a function trans(ttable, s). trans translates its string argument, s, according to its translation table argument, ttable. Argument ttable is, of course, a dictionary returned by themakettable function, and the result of trans is obtained by replacing each character, c, of s by ttable[c], if c is amongst the keys of the ttable. If however c is not in ttable.keys() then it is unchanged in the output. This is all harder to describe than to do! To easily look up k in dictionary, D, specifying that the same character is to be used if it is not present as a key in the dictionary you can use translation = D.get(k,k) # use help({}.get) to see more about get.
 
 def trans(ttable, s):
-    """Translates string s using the translation table"""
+    """Translates string s using translation table ttable"""
+    debug('trans: ' + '"'+s+'"')
+    translation = ""
     for c in s:
         translation += ttable.get(c,c)
+    debug(translation)
     return translation
 
 
 def testtrans():
     """Function to test translation code
-
-    Returns True if successful, False if any test fails
+       Returns True if successful, False if any test fails
     """
-
     ttable = makettable('abc', 'xyz')
     revttable = makettable('xyz', 'abc')
     tests = "Now I know my abc's"
     answer = "Now I know my xyz's"
 
-    if trans(ttable, tests) != answer: return False
-    if trans(revttable, trans(ttable, tests)) != "Now I know mb abc's": return False
-    if trans(ttable,'') != '': return False
-    if trans(makettable('',''), "abc") != 'abc': return False
-    return True
+    inputs = []
+    outputs = []
 
-# Define a function, histo(s) computing the histogram of a given string. (Look up histogram in the
-# dictionary if you don't know what it means.) The histogram returned by the function is a list of
-# characters in the input string s each paired with its frequency. Characters must appear in the list ordered
-# from most frequent to least frequent. For example, histo('implemented') is [('e',3),
-# ('m',2), ('d',1),('i',1), ('l',1), ('n',1), ('p',1), ('t',1)]. (Characters
-# with the same frequency must appear in increasing alphabetical order.) To implement the sorting you
-# must use the python built-in function sorted. Do not write your own sorting code. (Hint:
-# help(sorted) and the Python documentation are your friends.)
+    inputs.append([ttable, tests])
+    outputs.append(answer)
+
+    inputs.append([revttable, trans(ttable, tests)])
+    outputs.append("Now I know mb abc's")
+
+    inputs.append([ttable, ''])
+    outputs.append('')
+
+    inputs.append([makettable('',''), "abc"])
+    outputs.append('abc')
+
+    for i,o in zip(inputs,outputs):
+        test("trans(" + str(i).strip('[]') + ")", trans(i[0], i[1]), o)
+
+
+# Define a function, histo(s) computing the histogram of a given string. (Look up histogram in the dictionary if you don't know what it means.) The histogram returned by the function is a list of characters in the input string s each paired with its frequency. Characters must appear in the list ordered from most frequent to least frequent. For example, histo('implemented') is [('e',3), ('m',2), ('d',1), ('i',1), ('l',1), ('n',1), ('p',1), ('t',1)]. (Characters with the same frequency must appear in increasing alphabetical order.) To implement the sorting you must use the python built-in function sorted.
+# Do not write your own sorting code.
+# Hint: help(sorted) and the Python documentation are your friends.
 
 def histo(s):
-    """Given a string, returns a histogram"""
+    """Returns a histogram depicting the frequency of each char in string s"""
+    debug('histo: ' + '"'+s+'"')
     D = {}
     for c in s:
-        D[c] += 1
-    return sorted(D.items(), key=operator.itemgetter(1,0)) # sort by value then by key
+        D[c] = D.get(c, 0) + 1
+    # sorted by value then by key
+    # check that sort is most to least frequent, then alphabetical
+    histogram = sorted(D.items(), key=lambda t : (-t[1], t[0]))
+    debug(histogram)
+    return histogram
 
 
 def testhisto():
     """Function to test histrogram code
-
-    Returns True if successful, False if any test fails
+       Returns True if successful, False if any test fails
     """
 
-# A digraph is a pair of characters that occur adjacent to one another in a text. By convention we write
-# each digraph between a pair of '/' characters to make it easier to see where the blanks are. For
-# example the digraphs at the beginning of the first sentence of this section are /A /, / d/, /di/,
-# /ig/, etc. Digraph frequency counts are helpful in cryptological analysis of some ciphers.
-# Define a digraphs(s) function that returns a list containing the number of times each digraph occurs
-# in string s. Digraphs must be listed in alphabetical order. Again, use the built-in function sorted and do
-# not write your own sorting code. Digraphs that do not occur in the input (0 occurrences) should not be
-# listed in the output.
+    inputs = []
+    outputs = []
 
-def digraphs(s):
-    """Given a string, returns a digraph"""
-    D = {}
-    for i in range(len(s)-2):
-        D[s[i:i+2]] += 1
-    return sorted(D.items(), key=operator.itemgetter(0,1)) # sort by key then by value
+    inputs.append('implemented')
+    outputs.append([('e',3), ('m',2), ('d',1), ('i',1), ('l',1), ('n',1),
+                  ('p',1), ('t',1)])
+
+    inputs.append('abbccddd')
+    outputs.append([('d',3), ('b',2), ('c',2), ('a',1)])
+
+    inputs.append('aaabbccd')
+    outputs.append([('a',3), ('b',2), ('c',2), ('d',1)])
+
+    for i,o in zip(inputs,outputs):
+        test('histo(' + '"'+i+'"' + ')', str(histo(i)), str(o))
+
+
+# A digraph is a pair of characters that occur adjacent to one another in a text. By convention we write each digraph between a pair of '/' characters to make it easier to see where the blanks are. For example the digraphs at the beginning of the first sentence of this section are /A /, / d/, /di/, /ig/, etc. Digraph frequency counts are helpful in cryptological analysis of some ciphers. Define a digraphs(s) function that returns a list containing the number of times each digraph occurs in string s. Digraphs must be listed in alphabetical order. Again, use the built-in function sorted and do not write your own sorting code. Digraphs that do not occur in the input (0 occurrences) should not be listed in the output.
 
 # Here is what the function might return on some hypothetical sample input:
 #
@@ -107,40 +134,69 @@ def digraphs(s):
 # ('/or/', 36), ('/r /', 32), ('/re/', 44), ('/s /', 44), ('/t /',36),
 # ('/th/', 52), ('/to/', 42)
 
+def digraphs(s):
+    """Returns a digraph depicting the frequency of adjacent char in string s"""
+    debug('digraphs: ' + '"'+s+'"')
+    D = {}
+    for i in range(len(s)-2):
+        pair = '/' + s[i:i+2] + '/'
+        D[pair] = D.get(pair, 0) + 1
+    # sorted by key then by value
+    # check that sort is alphabetical, then be frequency
+    digraph = sorted(D.items(), key=lambda t : (t[0], -t[1]))
+    #digraph = sorted(D.items(), key=operator.itemgetter(0,1))
+    debug(digraph)
+    return digraph
+
+
 def testdigraphs():
     """Function to test digraph code
-
-    Returns True if successful, False if any test fails
+       Returns True if successful, False if any test fails
     """
-    print("testdigraph: not yet implemented")
+    inputs = []
+    outputs = []
+
+#
+# [('/ </', 48), ('/ a/', 56), ('/ d/', 30), ('/ i/', 34), ('/ o/', 37),
+# ('/ t/', 66), ('/. /', 31), ('/an/', 33), ('/co/', 47), ('/d /',38),
+# ('/de/', 44), ('/e /', 83), ('/he/', 41), ('/in/', 53), ('/n /',40),
+# ('/or/', 36), ('/r /', 32), ('/re/', 44), ('/s /', 44), ('/t /',36),
+# ('/th/', 52), ('/to/', 42)
+    inputs.append("The quick brown fox")
+    outputs.append([('/Th/', 1), ('/he/', 1), ('/e /', 1), ('/ q/', 1),
+                  ('/qu/', 1), ('/ui/', 1), ('/ic/', 1), ('/ck/', 1),
+                  ('/k /', 1), ('/ b/', 1), ('/br/', 1), ('/ro/', 1),
+                  ('/ow/', 1), ('/wn/', 1), ('/n /', 1), ('/ f/', 1),
+                  ('/fo/', 1), ('/ox/', 1)])
+
+    for i,o in zip(inputs,outputs):
+        test('digraphs(' + '"'+i+'"' + ')', str(digraphs(i)), str(o))
 
 
+#===================================== Main ====================================
 if __name__ == '__main__':
 
-    passedMsg = "%s passed"
-    failedMsg = "%s failed"
+    if debug:
+        passedMsg = "%s passed"
+        failedMsg = "%s failed"
 
-    if testttrans():
-        print ( passedMsg % 'testtrans' )
-    else:
-        print ( failedMsg % 'testtrans' )
+        print()
+        testtrans()
+        testhisto()
+        testdigraphs()
+        print()
 
 # etc. for the other tests.
 # notice how you are repeating a lot of code here
 # think about how you could avoid that -- we will discuss in class
 
+#===============================================================================
 
-#==============================================================================
-
-# Use your trans, histo, and digraph functions in an interactive python session to try to figure out
-# the following cryptogram. To make the functions available in the interactive session
+# Use your trans, histo, and digraph functions in an interactive python session to try to figure out the following cryptogram. To make the functions available in the interactive session
 #
 # >>> from one import *
 #
-# If you haven't solved one of these before you may want to look on the web for hints on the relative
-# frequencies of letters and digraphs in the English language. You can use your functions to try out
-# different possibilities and get clues about the translation being used. (Digits and punctuation are not
-# changed in this cryptogram.)
+# If you haven't solved one of these before you may want to look on the web for hints on the relative frequencies of letters and digraphs in the English language. You can use your functions to try out different possibilities and get clues about the translation being used. (Digits and punctuation are not changed in this cryptogram.)
 #
 # ZYX WVWUTSZRVQ VP OUNMXLX WKZYVQL WNXLXQZTK XLZSOTRLYXJ RQ
 # ZYX WSNI RL ZYX NXLUTZ VP SHHRJXQZST SQJ/VN RQZXQZRVQST NXTXSLXL OK
@@ -155,10 +211,7 @@ if __name__ == '__main__':
 # WTSQZL SQJ SQRMSTL GX'NX GVNIRQE JRTREXQZTK
 # ZV WNVZXHZ. (GGG.QWL.EVF/XFXN/QSZUNXLHRXQHX/OUNMXLXWKZYVQLRQZNV.YZM)
 #
-# Hint: use your makettable function to gradually build better and better translations between the
-# uppercase cryptogram characters to lowercase plaintext characters to make it visually apparent which
-# characters have been translated. Note numbers and punctuation in the cryptogram will appear
-# unchanged in the answer.
+# Hint: use your makettable function to gradually build better and better translations between the uppercase cryptogram characters to lowercase plaintext characters to make it visually apparent which characters have been translated. Note numbers and punctuation in the cryptogram will appear unchanged in the answer.
 
 thisIsTheCryptogramAnswer = """
 
