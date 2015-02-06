af9004a4b77408d5a93fb257943d32460aab6d06
James Jessen <jsjessen@gmail.com>
2015-02-06 11:57:03 -0800

    bit of planning

--- sps/two.py
+++ sps/two.py
@@ -1,2 +1,32 @@
 import sys
 import re #regular exressions
+
+# • IMPORTANT NOTE: implementing the functions required for the preliminary February 9 due date requires that you also design and implement the operand stack, dictionaries, and the dictionary stack.
+
+open(sys.argv[1]).readlines()
+
+# read lines from input file
+# tokenize
+# classify each token
+
+
+# class stack
+# class dict
+
+# • * built-in operators on numbers: add, sub, mul, div, eq, lt, gt
+#
+# • * built-in operators on boolean values: and, or, not; these take boolean operands only.
+#
+# • * built-in sequencing operators: if, ifelse; make sure that you understand the order of the
+#
+# • * stack operators: dup, exch, pop
+#
+# • * dictionary creation operator: dictz; takes no operands
+#
+# • * dictionary stack manipulation operators: begin, end. begin requires one dictionary operand on the operand stack; end has no operands.
+#
+# • * name definition operator: def. This requires two operands, a name and a value.
+#
+# • * stack printing operator (prints contents of stack without changing it): stack
+#
+# • * top-of-stack printing operator (pops the top element of the stack and prints it): =
